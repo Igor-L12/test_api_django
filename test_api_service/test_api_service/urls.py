2bea97b76c428346eac12324e4400aba82967be3
igor kenergy <lyubaev85@gmail.com>
2024-06-05 20:54:05 +0300

    fix pep8

--- test_api_service/test_api_service/urls.py
+++ test_api_service/test_api_service/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_service.urls')),
