367c1d698e6763ebaa731f08ce9c47a7fbff67f6
igor kenergy <lyubaev85@gmail.com>
2024-06-05 20:51:57 +0300

    pep8 fix

--- test_api_service/api_service/models.py
+++ test_api_service/api_service/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
+
 class Ad(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Заголовок объявления')
+    title = models.CharField(
+        max_length=255, 
+        verbose_name='Заголовок объявления'
+    )
     ad_id = models.IntegerField(unique=True, verbose_name='ID объявления')
     author = models.CharField(max_length=255, verbose_name='Автор объявления')
     views = models.IntegerField(verbose_name='Кол-во просмотров объявления')
     position = models.IntegerField(verbose_name='Позиция объявления')
 
     class Meta():
-        verbose_name='Объявление'
-        verbose_name_plural='Объявления'
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
 
     def __str__(self):
         return self.title
